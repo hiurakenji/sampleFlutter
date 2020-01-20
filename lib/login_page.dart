eb5121f09c5513b9afb220be691eea4e6a359a55
hiura <hiura.kenji@takefun.co.jp>
2020-01-20 14:50:26 +0900

    testcommit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,7 +10,7 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('ログイン画面'),
+        title: new Text('ログイン画面test'),
         leading: Container(),
       ),
       body: ListView(children: <Widget>[
